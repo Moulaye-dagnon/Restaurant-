9f6044ab78e4f27c201d1e4782f348c65df7efaf
Moulaye dagnon <moulayedagnon@gmail.com>
2025-03-16 17:09:20 +0000

    Added a new featuring

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-ratio = .3
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const choose = document.getElementById("number").value;
+  const date = document.getElementById("date").value;
+
+  alert("Mon nom est " + name + "et nous sommes " + choose + "pour le " + date);
+  console.log(inputV);
+  console.log(choose);
+});
+
+ratio = 0.3;
 const options = {
-    root: null,
-    rootMargin : '0px',
-    threshold : ratio
-}
-const callback = function (entries, observer){
-    entries.forEach((entry )=> {
-        if(entry.intersectionRatio > ratio){
-            console.log('visible')
-            entry.target.classList.remove('anim')
-            t.unobserve(r)
-        }
-        
-    });
-
-}
-const clas = document.querySelectorAll('.anim').forEach((r)=>{
-
-    const t = new IntersectionObserver(callback, options)
-    t.observe(r)
-})
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
+const callback = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > ratio) {
+      console.log("visible");
+      entry.target.classList.remove("anim");
+      t.unobserve(r);
+    }
+  });
+};
+const clas = document.querySelectorAll(".anim").forEach((r) => {
+  const t = new IntersectionObserver(callback, options);
+  t.observe(r);
+});
